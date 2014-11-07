43e8032c47d46914e1e691493a6f8eec073361ea
Brandon Dennis <toady00@gmail.com>
2014-11-06 21:12:14 -0500

    Simplifies EVERYTHING
    The original version of this was designed to generate events that
    actually made sense. For example, 3 events on the same card that all had
    the same uuid on the card, and the dates were chronological as far as
    the created date, update, deleted, etc.
    
    The intent was to make as acurate data as possible for testing
    elasticsearch performance. Elasticsearch is plenty fast enough after
    testing.
    
    Now I have changed the json schema to a flatter design, which should
    make searching even faster. But changing this code to rearrange the
    structure of the generated json was over complicated.
    
    This refactoring deletes much of the logic behind making data that makes
    sense, and now only generates one off events. There shouldn't ever be 2
    events with the same card uuid (unless there is a uuid collision). So
    technically none of the data would actually be bad, there's just no
    "series" of events any more.

---
+++ lib/event.rb
@@ -0,0 +1,57 @@
+require 'json'
+require 'securerandom'
+require 'date'
+
+class Event
+ 
+  ONE_DAY       = 1
+  ONE_MONTHS    = ONE_DAY    * 30
+  SIX_MONTHS    = ONE_MONTHS * 6
+  ONE_YEARS     = ONE_DAY    * 365
+  EIGHT_YEARS   = ONE_YEARS  * 8
+  SIXTEEN_YEARS = ONE_YEARS  * 16
+
+  class << self
+    def generate
+      event_hash.to_json
+    end
+
+    def event_hash
+      {
+        eventTarget: "card",
+        eventType: %w(created updated deleted).sample,
+        eventTimestamp: gen_timestamp,
+        eventTrigger: {
+          type: "currentUser",
+          uuid: SecureRandom.uuid
+        },
+        card: {
+          uuid: SecureRandom.uuid,
+          expiresOn: "2015-06-01T00:00:00+00:00",
+          firstName: gen_string,
+          lastName: gen_string,
+          kind: %w(staff player).sample,
+          avatar: gen_string,
+          birthdate: gen_birthdate,
+          status: %w(pending approved revoked).sample
+        }
+      }
+    end
+
+    def gen_date(range)
+      DateTime.now - range.to_a.sample
+    end
+
+    def gen_timestamp
+      gen_date(ONE_DAY..SIX_MONTHS)
+    end
+
+    def gen_birthdate
+      gen_date(EIGHT_YEARS..SIXTEEN_YEARS)
+    end
+
+    def gen_string
+      ('a'..'z').to_a.shuffle[0,(3..12).to_a.sample].join
+    end
+  end
+end
